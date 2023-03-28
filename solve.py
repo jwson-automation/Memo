adf5110ad1733d13e01b1b7fbe25486f94f1ba69
jwson-automation <sonjungwoo9@gmail.com>
2023-03-28 17:59:15 +0900

    update

--- solve.py
+++ solve.py
@@ -1,22 +1,31 @@
-def recur(idx):
-    
-    pass
+n, m, k = map(int, input().split())
 
+# 라이브러리
 
-n = int(input())
+# 라이센스, 프로젝트에서 쓸 수 있냐 없냐.
+# MiT -> 무조건 쓸 수 있음, GPL -> 상업적으로 사용하지 못한다.....
+# 기능적으로 맞아도 라이센스가 안맞으면 배포할 수 없음.
+# mvncenteral
+# jcenter
+# google
+# ....
 
-#물약 가격
-posion_price = list(map(int, input().split()))
+# ```
+# License
+# Copyright 2013 Square, Inc.
 
-#할인 정보
-info = {}
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-for posion in range(n):
-    info[posion] = []
-    p = int(input())
-    for _ in range(p):
-        a, d = map(int, input().split())
-        info[posion].append([a, d])
-    
-recur(0)
+# http: // www.apache.org/licenses/LICENSE-2.0
 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ```
+
+# 소송 걸릴 수 있음
+#
