bb7125ac0b3d7d8b4296edf27d69388076b48329
sonjungwoo <sonjungwoo9@gmail.com>
2023-04-12 14:10:37 +0900

    0412

--- solve.py
+++ solve.py
@@ -1,20 +1,26 @@
-import sys
-sys.setrecursionlimit(99999999)
+def recur(idx):
 
-def recur(n):
-    if n == 0:
-        return 0
+    if idx == 2 :
+        return False
 
-    if dp[n] != -1:
-        return dp[n]
+    cnt = 0
+    for i in range(3):
+
+        if recur(idx-i) == False:
+            cnt += 1
+
+    return recur(idx)
     
-    
-    dp[n] = min(recur(n-(i**2)) + 1 for i in range(1, int(n**0.5)+1))
-    return dp[n]
-
-
 n = int(input())
-dp = [-1 for _ in range(n+1)]
+numbers = [1,3,4]
+recur(n)
 
-print(recur(n))
-# print(dp)
+# 먼저 2로 만들 수 있는지 보기, 없다면 pass
+
+# 
+
+
+
+# 1,3,4를 버리고 줬을 때 2를 만들수 있냐 없냐를 꺼내야함.
+
+# 누군가가 2를 만들었다고 하면 그때 승패가 결정된다.
