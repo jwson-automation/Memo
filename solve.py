556b1ac82a635d7b0b07dd5da0d8305c2cd2b632
sonjungwoo <sonjungwoo9@gmail.com>
2023-04-14 18:01:42 +0900

    0414

--- solve.py
+++ solve.py
@@ -4,25 +4,20 @@ def recur(idx,pre):
     
     if idx > n :
         return -9999999
-    
-    # if idx == n :
-    #     return stair[idx]
-    
-    if dp[idx] != -1:
-        return dp[idx]
+        
+    if dp[idx][pre] != -1:
+        return dp[idx][pre]
 
-    dp[idx] = max(stair[idx],(recur(idx+1,pre+1) + stair[idx]), (recur(idx+2,1) + stair[idx]))
+    dp[idx][pre] = max(stair[idx],(recur(idx+1,pre+1) + stair[idx]), (recur(idx+2,1) + stair[idx]))
 
-    return dp[idx]
+    return dp[idx][pre]
 
 n = int(input())
 stair = [0]
-dp = [-1 for _ in range(n+1)]
+dp = [[-1,-1,-1] for _ in range(n+1)]
 for _ in range(n):
     score = int(input())
     stair.append(score)
 
 
 print(recur(0,0))
-print(dp)
-
