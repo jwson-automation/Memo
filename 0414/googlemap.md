f381ff3d95383cbb0524732399f863576e265e07
sonjungwoo <sonjungwoo9@gmail.com>
2023-04-14 09:06:16 +0900

    0414

---
+++ 0414/googlemap.md
@@ -0,0 +1,11 @@
+# Google Map
+
+구글 지도도 중요하지만 위치 정보를 가져오는 것이 중요합니다.
+
+## 위치 센서
+
+2가지 방법이 있습니다.
+
+1. GPS Provider
+
+2. Network Provider
--- solve.py
+++ solve.py
@@ -1,26 +1,25 @@
-def recur(idx):
-
-    if idx == 2 :
-        return False
-
-    cnt = 0
-    for i in range(3):
-
-        if recur(idx-i) == False:
-            cnt += 1
-
-    return recur(idx)
+def recur(idx,pre):
+    if pre > 1 :
+        return -9999999
     
+    if idx > n :
+        return -9999999
+    
+    if dp[idx] != -1:
+        return dp[idx]
+
+    dp[idx] = max((recur(idx+1,pre+1) + stair[idx]), (recur(idx+2,0) + stair[idx]))
+
+    return dp[idx]
+
 n = int(input())
-numbers = [1,3,4]
-recur(n)
+stair = [0]
+dp = [-1 for _ in range(n+1)]
+for _ in range(n):
+    score = int(input())
+    stair.append(score)
 
-# 먼저 2로 만들 수 있는지 보기, 없다면 pass
+answer = recur(1,0)
 
-# 
+print(answer)
 
-
-
-# 1,3,4를 버리고 줬을 때 2를 만들수 있냐 없냐를 꺼내야함.
-
-# 누군가가 2를 만들었다고 하면 그때 승패가 결정된다.
