7000dfcd2ac5c4af71fd5caaf7ddcc8324b0b92c
jwson <sonjungwoo9@gmail.com>
2023-05-17 22:55:37 +0900

    0517

--- solve.py
+++ solve.py
@@ -1,13 +1,35 @@
-import heapq
+import sys
+sys.setrecursionlimit(99999)
 
-array = [3,2,1,4,7,6,9,10,7,11,12,13,-1]
+input = sys.stdin.readline
 
-heapq.heapify(array)
+def dfs(node,prv):
+    for nxt in relation[node]:
+        if nxt == prv:
+            continue
 
-print(array)
+        # 자식에게 이동 -- ↑
+        dfs(nxt, node)
+        # 부모에게 이동 -- ↓
+
+        # 자식들의 숫자
+        child[node] += child[nxt]
+
+n,root,q = map(int,input().split())
+relation = [[] for _ in range(n+1)]
+child = [1 for _ in range(n+1)]
+
+# 입력
+for i in range(n-1):
+    a, b = map(int,input().split())
+    relation[a].append(b)
+    relation[b].append(a)
 
 
-# 1. 1,2,3,4,5,6,7,8,9
+# 출력
+dfs(root, -1)
 
-# 2. 1,2,3,4,5,6,9,8,7
 
+for _ in range(q):
+    answer = int(input())
+    print(child[answer])
