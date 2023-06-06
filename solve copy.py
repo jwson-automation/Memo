101b2041183d72402f35feca01459b029b2805d3
jwson <sonjungwoo9@gmail.com>
2023-06-07 01:28:56 +0900

    123

--- solve copy.py
+++ solve copy.py
@@ -1,44 +1,4 @@
-def dfs(node,prv):
-    # parent[node] = prv
-    # child[prv].append(node)
 
-    for nxt in relation[node]:
-        if nxt == prv:
-            continue
+# 네 카 라 쿠 배 당 토
 
-        # 부모 자식 관계
-        child[node].append(nxt)
-        parent[nxt] = node
-
-        # 깊이
-        depth[nxt] = depth[node] + 1
-
-
-        # 자식에게 이동 -- ↑
-        dfs(nxt,node)
-        # 부모에게 이동 -- ↓
-
-        # 자식들의 숫자
-        child_num[node] += child_num[nxt]
-
-
-n = int(input())
-relation = [[] for _ in range(n+1)]
-parent = [0 for _ in range(n+1)]
-child = [[] for _ in range(n+1)]
-depth = [0 for _ in range(n+1)]
-child_num = [1 for _ in range(n+1)]
-
-# 아스키 코드로 넣어주기
-for i in range(n-1):
-    a, b = map(int,input().split())
-    relation[a].append(b)
-    relation[b].append(a)
-
-# 전위 중위 후위
-dfs(1, 0)
-print("child :", child)
-print("parents : ", parent)
-
-print("depth :", depth)
-print("child_num : ", child_num)
+# 
--- solve.py
+++ solve.py
@@ -1,39 +1,47 @@
-# https://www.acmicpc.net/problem/17304
+n = int(input())
+arr = []
 
-def _find():
-    pass
-def _union():
-    pass
+y_list = []
+x_list = []
+ans = [-1]*n
 
-n, m = map(int,input().split())
-relation = [[] for _ in range(n+1)]
-double = [[] for _ in range(n+1)]
-visited = [0 for _ in range(n+1)]
+for _ in range(n):
+    a,b = map(int,input().split())
+    arr.append([a,b]) # x,y
+    y_list.append(b) # 14 15 14 16
+    x_list.append(a) # 14 16 15 15
 
-# 단방향 양방향 분리
-for _ in range(m):
-    a, b = map(int,input().split())
+for y in y_list:
+    for x in x_list:
+        dist = []
+        for ex,ey in arr: # 1개 기준으로, 계산합니다.
+            d = abs(ex-x) + abs(ey-y) # 두가지 x,y축을 향한 거리
+            dist.append(d)
 
-    if a in relation[b]:
-        relation[b].remove(a)
-        double[a].append(b)
-        double[b].append(a)
-    else:
-        relation[a].append(b)
+        dist.sort()
 
-# 단방향 방문 처리 완료
-for i in relation:
-    for j in i:
-        visited[j] = 1
+        print(f"dist:{dist}")
 
-# 양방향 그래프 트리 확인
-# 트리인가?
-# 사이클이 있는가?
-# 방문 처리가 포함 되어 있는 트리인가?
-for i in range(1, n+1):
-    pass
+        tmp = 0 
+        for i in range(len(dist)):
+            d = dist[i]
+            tmp += d
+            if ans[i] == -1: 
+                ans[i] = tmp
+            else :
+                ans[i] = min(tmp, ans[i])
+            print(f"ans: {ans}")
 
+print(*ans)
 
-print(relation)
-print(double)
-print(visited)
+# 점들의 최단거리는, 점들 속에 있다. 14, 15, 16, 100, 200 이라는 숫자가 있다면 최단 거리는 그중 하나인 16이다.
+# 그래서 먼저 만날 장소를 정해준다.
+# 만날 장소에 각각의 좌표가 오도록 만든다.
+# 가까운 순서대로 결과를 도출해낸다.
+
+# 0. A는 15 14, B는 15 16이다
+# 1. 15 14, 15 16 이라는 점에서 만날 수 있다.
+# 2. 15 14라는 곳에 A가 0이라는 거리로 올 수 있기 때문에, dist = [0]이 저장된다. 
+# 3. 15 14라는 곳에 A는 0으로 오고, 15 16은 2로 오기 떄문에 dist = [0,2]가 저장된다.
+# 4. 1개의 점이 모일 때 거리는 0, 2개의 점이 모일때 거리는 2가 된다.
+# 5. 여기에 정렬을 해주면 좌표의 숫자가 아무리 많아져도, 최소한의 거리가 되는 점들을 선택할 수 있게 된다.
